b69de20c059f884380927ab2ee9e08c00b95d44c
CodedLibra <adeola.ogundijo@yahoo.com>
2024-02-19 03:58:45 -0800

    feat: built Reviews section for landingpage and added  animation styles and assests

--- src/components/Pages/LandingPage/ReviewSection/index.tsx
+++ src/components/Pages/LandingPage/ReviewSection/index.tsx
@@ -1,18 +1,11 @@
 import { useEffect, useRef, useState } from 'react';
-import reviewerone from '../../../../assets/reviewerone.png'
-import reviewertwo from '../../../../assets/reviewertwo.jpg'
-import reviewerthree from '../../../../assets/reviewerthree.jpg'
 import Reviews from './Reviews'
 
-interface ReviewsProps {
-  title: string,
-  body: string,
-  img: string,
-  alt: string,
-  name: string,
-  role: string,
-  company: string,
-}
+/**
+ * Renders the review section with animations based on intersection with the viewport.
+ *
+ * @return {JSX.Element} The rendered review section component
+ */
 
 const ReviewSection = () => {
   const classes = ["animate-slideside300", "animate-slidein300", "animate-slidein500", "animate-slidein700"];
@@ -33,7 +26,6 @@ const ReviewSection = () => {
   }, [ref.current]); // Only re-create observer if ref changes
 
   useEffect(() => {
-    console.log(isIntersecting);
 
     if (isIntersecting) {
       ref.current?.querySelector('ul')
@@ -64,43 +56,10 @@ const ReviewSection = () => {
         </p>
       </div>
 
-      <ul className="flex flex-wrap animate-slideside300 opacity-0 container 2xl:max-w-[1200px] mx-auto gap-6 items-center justify-center">
-        {reviews.map((review, index) => (
-          <Reviews review={review} key={index} />
-        ))}
-      </ul>
+      <Reviews />
     </section>
   )
 }
 
 export default ReviewSection
 
-export const reviews: ReviewsProps[] = [
-  {
-    "title": "Data is everything & Gloed helps",
-    "body": "We think of it as everyone's responsibility in the organization to be more data-driven. After all, every single one of us is probably touching data in some way, regardless of your role",
-    "img": reviewerone,
-    "alt": "first reviewer image",
-    "name": "Gabriel Lages",
-    "role": "Business Intelligence and Analytics Manager",
-    "company": "Hotmart"
-  },
-  {
-    "title": "Satisfied User Here!",
-    "body": "On Gloed, you learn from the experts. As you are taking courses, you are really learning from the best instructors in the world.",
-    "img": reviewerthree,
-    "alt": "second reviewer image",
-    "name": "Rachel Alt-Simmons",
-    "role": "Head Of Strategic Design, Data, Pricing And Analytics",
-    "company": "AXA XL"
-  },
-  {
-    "title": "My team learners better now.",
-    "body": "As a manager, Gloed helps me show my team new packages and new ways to solve problems.",
-    "img": reviewertwo,
-    "alt": "third reviewer image",
-    "name": "Ofentswe Lebogo",
-    "role": "Data Scientist",
-    "company": "Council for Scientific and Industrial Research (CSIR)"
-  },
-]
