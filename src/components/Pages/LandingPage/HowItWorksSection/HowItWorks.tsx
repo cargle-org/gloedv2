170f1ebf956bbf4b433451a9fc2ee0ce8365a4c7
CodedLibra <adeola.ogundijo@yahoo.com>
2024-02-19 03:49:47 -0800

    feat: built HowItWorks section for landingpage and added styles and asses

---
+++ src/components/Pages/LandingPage/HowItWorksSection/HowItWorks.tsx
@@ -0,0 +1,53 @@
+import Card from "../../../UI/Card"
+import howitworksone from '../../../../assets/how_it_works_one.svg'
+import howitworkstwo from '../../../../assets/how_it_works_two.svg'
+import howitworksthree from '../../../../assets/how_it_works_three.svg'
+
+interface HowItWorksProps {
+  step: number,
+  figImg: string,
+  figCaption: string
+}
+
+const HowItWorksSection = () => {
+  return (
+    <ul className=" flex flex-wrap md:flex-nowrap justify-between space-y-10 md:space-y-0 gap-6 lg:gap-12 xl:gap-6 animate-slideside300 opacity-0 md:w-[600px] lg:w-[710px] xl:w-[900px]">
+      {howItWorks.map((step, index) => (
+        <li key={index} className='flex w-full flex-col items-center gap-1 md:gap-6 space-y-2'>
+          <Card variant='primary' className='w-[200px] h-[200px] md:w-[150px] md:h-[150px] lg:w-[170px] lg:h-[170px] xl:w-[200px] xl:h-[200px]'>
+            <span className="flex relative bottom-[30px] left-[25px] font-semibold place-self-start items-center justify-center text-white tracking-[-0.72px] leading-[36px] text-2xl w-[50px] h-[50px] flex-shrink-0 bg-primary rounded-full">
+              {index + 1}
+            </span>
+            <figure className="w-full flex-shrink-0 rounded-2xl ">
+              <img src={step.figImg} className="w-full h-full object-cover " alt="How it works guide one" />
+            </figure>
+          </Card>
+          <div className="w-[181px] text-wrap text-center text-xl font-semibold text-black leading-extra-loose tracking-tight">
+            {step.figCaption}
+          </div>
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+export default HowItWorksSection;
+
+export const howItWorks: HowItWorksProps[] = [
+  {
+    "step": 1,
+    "figImg": howitworksone,
+    "figCaption": `Create your Gloed learner’s account.`
+  },
+  {
+    "step": 2,
+    "figImg": howitworkstwo,
+    "figCaption": "Select your course type and topic."
+  },
+  {
+    "step": 3,
+    "figImg": howitworksthree,
+    "figCaption": "Attend your Classes!"
+  },
+]
+
---
+++ src/components/Pages/LandingPage/HowItWorksSection/index.tsx
@@ -0,0 +1,69 @@
+import { useState, useRef, useEffect } from 'react';
+import HowItWorks from './HowItWorks'
+
+const HowItWorksSection = () => {
+  const classes = ["animate-slideside300", "animate-slidein300", "animate-slidein500", "animate-slidein700"];
+  const [isIntersecting, setIsIntersecting] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => setIsIntersecting(entry.isIntersecting),
+      { rootMargin: '-150px' }
+    );
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => observer.disconnect();
+  }, [ref.current]); // Only re-create observer if ref changes
+
+  useEffect(() => {
+    console.log(isIntersecting);
+
+    if (isIntersecting) {
+      ref.current?.querySelector('ul')
+        ?.classList.add(classes[0]);
+      ref.current?.querySelector('h1')
+        ?.classList.add(classes[1]);
+      ref.current?.querySelector('h2')
+        ?.classList.add(classes[2]);
+      ref.current?.querySelector('p')
+        ?.classList.add(classes[3]);
+    }
+    else {
+      ref.current?.querySelector('ul')
+        ?.classList.remove(classes[0]);
+      ref.current?.querySelector('h1')
+        ?.classList.remove(classes[1]);
+      ref.current?.querySelector('h2')
+        ?.classList.remove(classes[2]);
+      ref.current?.querySelector('p')
+        ?.classList.remove(classes[3]);
+    }
+  }, [isIntersecting]);
+
+  return (
+    <section ref={ref} className="font-plus-jakarta-sans bg-white py-[80px] sm:py-[74px] md:py-[120px] space-y-14 md:space-y-20">
+      <div className="container lg:max-w-[712px] mx-auto space-y-3">
+        <h1 className="text-center text-xl font-semibold text-[#757575] animate-slidein300 opacity-0 leading-extra-loose tracking-tight">
+          HOW TO GET STARTED
+        </h1>
+        <h2 className="text-center mx-1 text-[27px] sm:text-[30px] md:text-[40px] text-[#0D121F] font-bold animate-slidein500 opacity-0 md:tracking-[-1.2px] md:leading-xl-loose">
+          Getting Started is so simple!
+        </h2>
+        <p className="text-center mx-1.5 font-normal text-xl sm:text-[21px] text-[#3636363] animate-slidein700 opacity-0 leading-extra-loose tracking-tight">
+          With Gloed, you can decide to sign up for a 1:1 class and have a time scheduled with an instructor or you can sign up for general group classes!
+        </p>
+      </div>
+
+      <div className=" flex items-center justify-center mx-auto container ">
+        <HowItWorks />
+      </div>
+    </section>
+  )
+};
+
+export default HowItWorksSection
+
