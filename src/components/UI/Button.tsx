1cddb637725af09c03685f2943d5158f05d52e88
CodedLibra <adeola.ogundijo@yahoo.com>
2024-02-20 00:44:52 -0800

    styles: added styles to disable buttons and added margin top to create class button

--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -9,8 +9,8 @@ interface ButtonProps {
 
 
 const Button: React.FC<ButtonProps> = (props) => {
-  const btn = 'flex items-center text-center text-nowrap transform transition hover:-translate-y-0.5 focus:outline-none focus:ring focus:ring-offset-0';
-  const btnPrimary = 'justify-center font-plus-jakarta-sans px-6 py-3 text-white text-sm sm:text-base font-semibold bg-primary rounded-4xl leading-6 tracking-[-0.32px] hover:bg-primary-light focus:ring-primary-dark focus:ring-opacity-50 active:bg-primary-dark ';
+  const btn = 'flex items-center text-center text-nowrap transform transition hover:-translate-y-0.5 focus:outline-none focus:ring focus:ring-offset-0 disabled:opacity-50 disabled:cursor-not-allowed';
+  const btnPrimary = 'justify-center font-plus-jakarta-sans px-6 py-3 text-white text-sm sm:text-base font-semibold bg-primary rounded-4xl leading-6 tracking-[-0.32px] hover:bg-primary-light focus:ring-primary-dark focus:ring-opacity-50 active:bg-primary-dark';
   const btnOptions = 'justify-center py-1 px-4 2xl:px-5 lg:px-3.5 font-Inter sm:text-sm md:font-semibold sm:font-medium lg:text-base text-white rounded-full'
   const btnAside = 'justify-center px-4 py-2.5 font-Poppins bg-primary font-medium text-sm text-white rounded-[4px]'
   const btnDark = 'justify-center border border-black py-1.5 sm:py-2.5 px-2 sm:px-4 lg:px-3.5 shadow-sm shadow-[#100e280d] rounded-lg bg-black font-Inter text-base lg:text-sm xl:text-base font-semibold leading-6 text-white focus:ring-black items-center'
@@ -22,6 +22,7 @@ const Button: React.FC<ButtonProps> = (props) => {
 
   return (
     <button
+      disabled
       type={props.type || "button"}
       onClick={props.onClick}
       className={`
