96d94e9eeaf403f03a021e24fdb34a088beab872
Thrila <Ifeanyindubizu503@gmail.com>
2024-03-28 02:48:58 +0100

    chore: linting
    adhered to code formatting

--- src/components/Pages/PrivacyPolicy.tsx
+++ src/components/Pages/PrivacyPolicy.tsx
@@ -11,54 +11,58 @@ const PrivacyPolicy = () => {
         <p className="text-gray-700 mt-2 text-[12px] sm:text-inherit">
           Last updated 20th May, 2022.
         </p>
-        <h1 className="text-xl mt-5 text-left font-bold text-red-500">
-          Introduction
-        </h1>
-        <p className="text-gray-700 mt-2 text-[16px] sm:text-inherit">
-          <a className="text-red-400" href="https://Gloed.co">Gloed.co</a>  Limited is an educational institution, licensed by the Federal Government of Nigeria, with the RC <b>1953165</b> Gloed operates the gloed.co web application.
-        </p>
-        <p className="text-gray-700 mt-2 text-[16px] sm:text-inherit">
-          This page is used to inform website visitors regarding our policies with the collection, use, and disclosure of Personal Information if anyone decided to use our Service, the Gloed website.</p>
-
-        <p className="text-gray-700 mt-2 text-[16px] sm:text-inherit">
-          If you choose to use our Service, then you agree to the collection and use of information in relation with this policy. The Personal Information that we collect are used for providing and improving the Service. We will not use or share your information with anyone except as described in this Privacy Policy. </p>
-
-        <h1 className="text-xl mt-5 text-left font-bold text-red-500">
-          Information Collection and Use
-        </h1>
-        <p className="text-gray-700 mt-2 text-[16px] sm:text-inherit">
-          For a better experience while using Gloed.co, we may require you to provide us with certain personally identifiable information, including but not limited to your name, phone number, and email address. The information that we collect will be used to contact or identify you.  </p>
-
-        <p className="text-gray-700 mt-2 text-[16px] sm:text-inherit">
-          We currently request that you only provide your name, phone number and email address upon class registration.       </p>
+        <div className="mx-5">
 
 
-        <h1 className="text-xl mt-5 text-left font-bold text-red-500">
-          Protection of Privacy
-        </h1>
+          <h1 className="text-xl mt-5 text-left font-bold text-red-500">
+            Introduction
+          </h1>
+          <p className="text-gray-700 mt-2 text-[16px] sm:text-inherit">
+            <a className="text-red-400" href="https://Gloed.co">Gloed.co</a>  Limited is an educational institution, licensed by the Federal Government of Nigeria, with the RC <b>1953165</b> Gloed operates the gloed.co web application.
+          </p>
+          <p className="text-gray-700 mt-2 text-[16px] sm:text-inherit">
+            This page is used to inform website visitors regarding our policies with the collection, use, and disclosure of Personal Information if anyone decided to use our Service, the Gloed website.</p>
 
-        <p className="text-gray-700 mt-2 text-[16px] sm:text-inherit">
-          Gloed.co safeguards the security of the data you provide us with physical, electronic, and managerial procedures. Please note that we cannot guarantee that any data transmitted over the Internet is completely secure. Accordingly, we encourage you to take every precaution to protect your personal data when you are on the Internet.
-        </p>
+          <p className="text-gray-700 mt-2 text-[16px] sm:text-inherit">
+            If you choose to use our Service, then you agree to the collection and use of information in relation with this policy. The Personal Information that we collect are used for providing and improving the Service. We will not use or share your information with anyone except as described in this Privacy Policy. </p>
 
-        <h1 className="text-xl mt-5 text-left font-bold text-red-500">
-          Commitment to Privacy
-        </h1>
+          <h1 className="text-xl mt-5 text-left font-bold text-red-500">
+            Information Collection and Use
+          </h1>
+          <p className="text-gray-700 mt-2 text-[16px] sm:text-inherit">
+            For a better experience while using Gloed.co, we may require you to provide us with certain personally identifiable information, including but not limited to your name, phone number, and email address. The information that we collect will be used to contact or identify you.  </p>
 
-        <p className="text-gray-700 mt-2 text-[16px] sm:text-inherit">
-          To make sure your personal information remains confidential; we communicate these privacy guidelines to every Spikk employee. Spikk’s Website may, from time to time, contain links to other sites. Spikk does not share your personal information with those websites and is not responsible for their privacy practices. We encourage you to learn about the privacy policies of any such company. If we are going to use your personal information differently from that stated at the time of collection, we will inform you accordingly. Spikk’s Privacy Policy is subject to change at any time. It is in your interest to review the privacy policy regularly for any changes.
-        </p>
-
-
-        <h1 className="text-xl mt-5 text-left font-bold text-red-500">
-          Contact Us
-        </h1>
+          <p className="text-gray-700 mt-2 text-[16px] sm:text-inherit">
+            We currently request that you only provide your name, phone number and email address upon class registration.       </p>
 
 
-        <p className="text-gray-700 mt-2 pb-5 text-[16px] sm:text-inherit">
-          If you have any questions or suggestions about our Privacy Policy, do not hesitate to <a className="text-red-400" href="http://gloed.co">Contact Us</a>.
-        </p>
-      </div >
+          <h1 className="text-xl mt-5 text-left font-bold text-red-500">
+            Protection of Privacy
+          </h1>
+
+          <p className="text-gray-700 mt-2 text-[16px] sm:text-inherit">
+            Gloed.co safeguards the security of the data you provide us with physical, electronic, and managerial procedures. Please note that we cannot guarantee that any data transmitted over the Internet is completely secure. Accordingly, we encourage you to take every precaution to protect your personal data when you are on the Internet.
+          </p>
+
+          <h1 className="text-xl mt-5 text-left font-bold text-red-500">
+            Commitment to Privacy
+          </h1>
+
+          <p className="text-gray-700 mt-2 text-[16px] sm:text-inherit">
+            To make sure your personal information remains confidential; we communicate these privacy guidelines to every Spikk employee. Spikk’s Website may, from time to time, contain links to other sites. Spikk does not share your personal information with those websites and is not responsible for their privacy practices. We encourage you to learn about the privacy policies of any such company. If we are going to use your personal information differently from that stated at the time of collection, we will inform you accordingly. Spikk’s Privacy Policy is subject to change at any time. It is in your interest to review the privacy policy regularly for any changes.
+          </p>
+
+
+          <h1 className="text-xl mt-5 text-left font-bold text-red-500">
+            Contact Us
+          </h1>
+
+
+          <p className="text-gray-700 mt-2 pb-5 text-[16px] sm:text-inherit">
+            If you have any questions or suggestions about our Privacy Policy, do not hesitate to <a className="text-red-400" href="http://gloed.co">Contact Us</a>.
+          </p>
+        </div >
+      </div>
 
     </div >
   );
