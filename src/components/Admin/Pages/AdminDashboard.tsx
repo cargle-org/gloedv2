2c0d2714a0b603eaa8b855f4a6b72a5eb654d9b1
CodedLibra <adeola.ogundijo@yahoo.com>
2024-04-26 01:26:12 -0700

    feat: added AdminSideBar and refactored AdminDashboard features

--- src/components/Admin/Pages/AdminDashboard.tsx
+++ src/components/Admin/Pages/AdminDashboard.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import AdminSideBar from '../Layout/AdminSideBar'
 import AdminCourseOptionsButton from '../Layout/AdminDashboard/AdminCourseOptionsButton';
 import { AdminDashboardCourseLists } from '../../Layout/DashboardCourseLists';
@@ -6,34 +6,60 @@ import AdminDashboardCards from '../Layout/AdminDashboard/AdminDashboardCards';
 import Button from '../../UI/Button';
 import CreateClass from '../Layout/Modals/CreateClass';
 import CreateTest from '../Layout/Modals/CreateTest';
+import LoadingSpinner from '../../UI/LoadingSpinner';
+import useHttp from '../../hooks/use-https';
+import { getAllClassesRequest } from '../../../utils/class-fetch';
 
-const AdminDashboard = () => {
+const AdminDashboard: React.FC = () => {
   const [isAddClass, setIsAddClass] = useState(false)
-  const [isAddTest, setIsAddTest] = useState(false)
+  const [isAddTest, setIsAddTest] = useState(false);
+
+  const { sendRequest, status, error, data: courseLists } =
+    useHttp(getAllClassesRequest);
+
+
+  useEffect(() => {
+    sendRequest();
+  }, [sendRequest]);
+
+  //render loading indicator
+  let authState;
+
+  if (status === 'pending') {
+    authState = (
+      <LoadingSpinner />
+    );
+  }
+
+  //Handle "Unauthorized" server error
+  if (status === "pending" && error?.toString().includes("Unauthorized")) {
+    window.location.reload();
+  }
 
   //Create class
   const showAddClass = () => {
     setIsAddClass(true)
   }
-
   const hideAddClass = () => {
     setIsAddClass(false)
   }
 
   //Create test
-  const showAddTest = () => {
-    setIsAddTest(true)
-  }
-
+  // const showAddTest = () => {
+  //   setIsAddTest(true)
+  // }
   const hideAddTest = () => {
     setIsAddTest(false)
   }
 
   return (
-    <main className="flex container mx-auto max-w-[300px] sm:max-w-[400px] md:max-w-[610px] lg:max-w-[700px] xl:max-w-full 2xl:max-w-[1440px] mb-20 lg:mt-2">
-      <AdminSideBar />
-      <div className='flex flex-col flex-wrap gap-4  mx-auto 2xl:gap-0 xl:flex-nowrap max-w-full xl:max-w-[970px] 2xl:max-w-6xl 3xl:max-w-7xl'>
-        <section className='flex justify-between items-center mb-2 sm:mb-4 xl:mb-7 flex-col-reverse md:flex-row max-w-[850px] 2xl:max-w-4xl '>
+    <div className="flex container mx-auto max-w-[300px] sm:max-w-[400px] md:max-w-[610px] lg:max-w-[700px] xl:max-w-full 2xl:max-w-[1440px] mb-20 lg:mt-2">
+      <AdminSideBar isOpen={false} />
+      <main className='flex flex-col flex-wrap gap-4 2xl:gap-0 xl:flex-nowrap mx-auto max-w-full xl:max-w-[970px] 2xl:max-w-6xl 3xl:max-w-7xl'>
+        <section className='flex justify-between items-center flex-col-reverse md:flex-row max-w-[850px] 2xl:max-w-4xl mb-2 sm:mb-4 xl:mb-7'>
+          {/* ***UI state*** */}
+          {authState}
+
           <h3 className="flex py-2.5 items-start text-xl xl:text-2xl font-Poppins font-medium leading-[42px]">
             Welcome back, Super Admin
           </h3>
@@ -41,20 +67,19 @@ const AdminDashboard = () => {
             <Button onClick={showAddClass} variant='dark'>
               Create class
             </Button>
-            <Button onClick={showAddTest} variant='dark'>
+            <Button variant='dark' disabled={true}>
               Create test
             </Button>
           </div>
         </section>
-
         {isAddClass && <CreateClass onClose={hideAddClass} />}
         {isAddTest && <CreateTest onClose={hideAddTest} />}
 
         <AdminDashboardCards />
         <AdminCourseOptionsButton />
-        <AdminDashboardCourseLists />
-      </div>
-    </main >
+        <AdminDashboardCourseLists classes={courseLists} />
+      </main>
+    </div >
   )
 }
 
