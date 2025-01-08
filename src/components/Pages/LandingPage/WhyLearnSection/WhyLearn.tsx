39af006b33e4cfa2fc4b4adbcb54e028f5734c2b
CodedLibra <adeola.ogundijo@yahoo.com>
2025-01-08 16:52:39 +0100

    styles: added hover state styles

--- src/components/Pages/LandingPage/WhyLearnSection/WhyLearn.tsx
+++ src/components/Pages/LandingPage/WhyLearnSection/WhyLearn.tsx
@@ -4,6 +4,7 @@ interface WhyLearnProps {
   borderStyle: string;
   titleColor: string;
   positionStyle?: string;
+  hoverStyle: string;
 }
 
 /**
@@ -18,13 +19,13 @@ const WhyLearn = () => {
       {textElements.map((text, id) => (
         <li
           key={id}
-          className={`flex mx-auto p-6 items-start gap-6 md:gap-8 border-[1px] md:border-[0.5px] rounded-lg ${text.borderStyle} ${text.positionStyle}`}
+          className={`flex mx-auto p-6 items-start gap-6 md:gap-8 border-[1px] md:border-[0.5px] rounded-lg ${text.borderStyle} ${text.positionStyle} ${text.hoverStyle}`}
         >
           <div className={`flex flex-col gap-3 items-start ${text.titleColor}`}>
             <div className="text-base md:text-xl font-bold leading-[26px]">
               {text.title}
             </div>
-            <div className="text-[#212121] text-sm md:text-xl md:text-wrap font-[274] leading-6 md:leading-8">
+            <div className="text-[#212121] hover:text-white text-sm md:text-xl md:text-wrap font-[274] leading-6 md:leading-8">
               {text.description}
             </div>
           </div>
@@ -39,7 +40,8 @@ export default WhyLearn;
 const textElements: WhyLearnProps[] = [
   {
     title: "Comprehensive Curriculum",
-    titleColor: "text-[#4588EC]",
+    titleColor: "text-[#4588EC] hover:text-white",
+    hoverStyle: "hover:bg-[#4588EC]",
     borderStyle: "border-[#4588EC80]",
     description:
       "Dive deep into a curriculum designed for all skill levels, from beginner to advanced. Learn at your own pace or accelerate your progress through personalized 1:1 sessions.",
@@ -47,14 +49,16 @@ const textElements: WhyLearnProps[] = [
   {
     title: "Hands-On Learning",
     borderStyle: "border-[#9747FF80]",
-    titleColor: "text-[#9747FF]",
+    titleColor: "text-[#9747FF] hover:text-white",
+    hoverStyle: "hover:bg-[#9747FF]",
     description:
       "Apply theoretical knowledge directly within SQL and Excel environments. Engage in real-world scenarios that help you develop practical, job-ready skills.",
   },
   {
     title: "Expert-Led Instruction",
     borderStyle: "border-[#1FAF3880]",
-    titleColor: "text-[#1FAF38]",
+    titleColor: "text-[#1FAF38] hover:text-white",
+    hoverStyle: "hover:bg-[#1FAF38] ",
     positionStyle:
       "col-span-full place-self-center xl:col-span-1 xl:place-self-auto",
     description:
